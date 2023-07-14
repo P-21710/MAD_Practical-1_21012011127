8cae4693ac27f661c6d29be4647d22f5e78aa0b2
student <student@gnu.ac.in>
2023-07-14 08:46:16 +0530

    Created practical-1.1.kt

---
+++ src/main/kotlin/practical-1.1.kt
@@ -0,0 +1,22 @@
+fun main()
+{
+    println("Hello World")
+    var a:Int =10
+    var b:String="hello"
+    var c:Double=21.23
+    var d:Float=11.2f
+    var e:Long=2235664
+    var f:Char='T'
+    var g:Short=-2
+    var h:Boolean=true
+    var i:Byte=2
+    println("$a is integer value")
+    println("$b is string value")
+    println("$c is double value")
+    println("$d is float value")
+    println("$e is long value")
+    println("$f is character value")
+    println("$g is short value")
+    println("$h is boolean value")
+    print("$i is byte value")
+}
---
+++ src/main/kotlin/practical-1.2.kt
@@ -0,0 +1,8 @@
+fun main()
+{
+    var a:Int=5
+    println("$a is intege value")
+    var b=a.toDouble()
+    println("$b is double value")
+
+}
