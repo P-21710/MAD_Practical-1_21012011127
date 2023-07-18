f0526f5d83140f2dc9dae9ab343307124599c2d4
Student <student@gnu.ac.in>
2023-07-18 08:56:07 +0530

    created practical 1.4

---
+++ src/main/kotlin/practical-1.4.kt
@@ -0,0 +1,9 @@
+fun main()
+{
+    println("Enter the number")
+    var a= readLine()!!.toInt()
+    if (a%2==0)
+        println("the number is even")
+    else
+        println("the number is odd")
+}
