f7c8b667740653ed2643354671f9e8a01dd7079a
Student <student@gnu.ac.in>
2023-07-18 08:21:37 +0530

    created practical 1.2 and 1.3

--- src/main/kotlin/practical-1.2.kt
+++ src/main/kotlin/practical-1.2.kt
@@ -4,5 +4,12 @@ fun main()
     println("$a is intege value")
     var b=a.toDouble()
     println("$b is double value")
-
+    var c:String="10"
+    println("$c is string value")
+    var d=c.toInt()
+    println("$d is integer value")
+    var e:String="11"
+    println("$e is string value")
+    var f=e.toDouble()
+    println("$f is double value")
 }
---
+++ src/main/kotlin/practical-1.3.kt
@@ -0,0 +1,5 @@
+fun main()
+{
+    println("enter student enrollment number")
+
+}
