880f910612d44e861567a7cbb96a65b2358cfda8
Student <student@gnu.ac.in>
2023-07-21 08:57:18 +0530

    creating 1.8

---
+++ src/main/kotlin/practical-1.8.kt
@@ -0,0 +1,27 @@
+import com.sun.glass.ui.Size
+import java.util.*
+
+fun main()
+{    /*
+     println("1.Create an array1 using array of method:")
+    var arr1 = arrayOf(10,20,35,45,50)
+    println(arr1.contentDeepToString())
+    */
+    /*
+    println("2.create array using Array<>():")
+    var arr2 = Array<Int>(5){0}
+    println(arr2.contentDeepToString())
+    */
+    /*
+    println("3.Creating an array using lambda function")
+    var arr3 = Array<Int>(7){i: Int -> i }
+    println(arr3.contentDeepToString())
+    */
+    /*
+    println("4.creating array by using intarray()")
+    var arr4 = IntArray(5){i: Int ->i}
+    println(arr4.contentToString())
+    */
+    println("5.creating array by using intarrayof()")
+    var arr5 = intArrayOf(5)
+}
