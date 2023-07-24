0a2f92cbb6624991180564949be7cfa2da46f6d5
P-21710 <98374171+P-21710@users.noreply.github.com>
2023-07-24 21:55:52 +0530

    Full practical-1

--- src/main/kotlin/practical-1.8.kt
+++ src/main/kotlin/practical-1.8.kt
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 
 fun main() {    /*
-=======
-import com.sun.glass.ui.Size
-import java.util.*
-
-fun main()
-{    /*
->>>>>>> origin/master
      println("1.Create an array1 using array of method:")
     var arr1 = arrayOf(10,20,35,45,50)
     println(arr1.contentDeepToString())
@@ -27,78 +19,32 @@ fun main()
     var arr4 = IntArray(5){i: Int ->i}
     println(arr4.contentToString())
     */
-<<<<<<< HEAD
     /*
     println("5.creating array by using intarrayof()")
-    var arr5 = intArrayOf(5)
-    */
-    /*
-    println("6.Array created by using arrayof() and intArrayOf()")
-    var arr6 = arrayOf(intArrayOf(21, 23), intArrayOf(24, 11), intArrayOf(39, 12))
-    println(arr6.contentDeepToString())
+    var arr5 = intArrayOf(5,4,3)
+    println(arr5.contentToString())
+
      */
     /*
-    println("6.user input in array")
+    println("Array created by using arrayof() and intArrayOf()")
+    var a6 = arrayOf(intArrayOf(45,3), intArrayOf(4,1), intArrayOf(9,2))
+    println(a6.contentDeepToString())
+*/
+    println("Array created by using arrayof() and intArrayOf()")
+    var a6 = arrayOf(intArrayOf(45,3), intArrayOf(4,1), intArrayOf(9,2))
+    println(a6.contentDeepToString())
+
     print("Enter number of Elements : ")
     val size : Int = readLine()!!.toInt()
-    val arr7 = IntArray(size) {0}
+    val user = IntArray(size) {0}
 
 
     for(i in 0 until size)
     {
         print("a[$i]:")
-        arr7[i] = readLine()!!.toInt()
+        user[i] = readLine()!!.toInt()
     }
     print("Entered Array = ")
-    println(arr7.contentToString())
-    */
-    /*
-    println("7.sorting of array with builtin function")
-    print("Enter number of Elements : ")
-    val size : Int = readLine()!!.toInt()
-    val arr = IntArray(size) {0}
+    println(user.contentToString())
 
-
-    for(i in 0 until size)
-    {
-        print("a[$i]:")
-        arr[i] = readLine()!!.toInt()
-    }
-    print("Entered Array = ")
-    println(arr.contentToString())
-    arr.sort()
-    println("Sorted array: ${arr.joinToString(" ")}")
-     */
-    println("sorting of array without builtin function")
-    fun bubbleSort(arr: IntArray) {
-        val n = arr.size
-        var temp: Int
-        for (i in 0 until n - 1) {
-            for (j in 0 until n - i - 1) {
-                if (arr[j] > arr[j + 1]) {
-                    temp = arr[j]
-                    arr[j] = arr[j + 1]
-                    arr[j + 1] = temp
-                }
-            }
-        }
-    }
-    println("Enter the numbers of elements:")
-        val input = readLine()
-        val inputArray = input?.split(" ")?.map { it.toInt() }?.toIntArray()
-        if (inputArray == null) {
-            println("Invalid input. Please enter numeric elements separated by spaces.")
-            return
-        }
-        bubbleSort(inputArray)
-        println("Sorted array: ${inputArray.joinToString(" ")}")
-    }
-
-
-
-
-=======
-    println("5.creating array by using intarrayof()")
-    var arr5 = intArrayOf(5)
 }
->>>>>>> origin/master
